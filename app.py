a9e2b8f922ebcb4effb64c1c30b74357ff8fe3f1
Ziad Abouelfarah <zain.fake02@gmail.com>
2020-03-24 23:25:25 +0100

    expa grabber v1

---
+++ app.py
@@ -0,0 +1,49 @@
+# javascript:prompt("Access Token",/access_token(?:%[A-F0-9]{2}){3}(.{64})/.exec(document.cookie)[1]);void(0);
+
+from flask import Flask, jsonify, request
+import requests
+import pprint
+
+app = Flask(__name__)
+
+
+def findNumber(country_code, number):
+    if country_code != None and number != None:
+        if len(number) == 10 and country_code == "+212":
+            return country_code + number[1:]
+        else:
+            return country_code + number
+    else:
+        return False
+    
+
+
+@app.route("/")
+def index():
+    access_token = request.args.get('access_token', type = str)
+    #print(access_token)
+    if access_token == None: 
+        return jsonify({"error": "No access token"}), 400
+    else:
+        data = requests.get("https://gis-api.aiesec.org/v2/people?access_token={}".format(access_token))
+
+        if data.status_code == 401: 
+            return jsonify({"error": "invalid access token"}), 401
+        else:
+            data = data.json()["data"]
+            modifiedArray = []
+            for neeko in data:
+                katarina = {}
+                katarina["Name"] = neeko["full_name"]
+                katarina["Email"] = neeko["email"]
+                # print(neeko["country_code"], neeko["phone"])
+                # pprint.pprint(neeko)
+                katarina["Phone"] = findNumber(neeko["country_code"], neeko["phone"])
+                if katarina["Phone"] == False:
+                    pass
+                else: 
+                    modifiedArray.append(katarina)
+
+            return jsonify(modifiedArray), 200
+
+app.run(debug=True)
